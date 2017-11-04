bb7637b838b4c47bfca868e40e6300acdd90f662
DanielsLuz <danielluz1995@gmail.com>
2017-11-03 22:23:14 -0200

    Changed initialization params.

--- spec/modules/process_unit_spec.rb
+++ spec/modules/process_unit_spec.rb
@@ -1,14 +1,5 @@
 describe ProcessUnit do
-  subject {
-    ProcessUnit.new(init_time: 2,
-                    priority: 0,
-                    processor_time: 7,
-                    memory_blocks: 64,
-                    printer: 1,
-                    scanner: 0,
-                    modem: 0,
-                    num_code_disk: 0)
-  }
+  subject { ProcessUnit.new(2, 0, 7, 64, 1, 0, 0, 0) }
 
   it { is_expected.to be_a ProcessUnit }
 
--- src/modules/process_unit.rb
+++ src/modules/process_unit.rb
@@ -1,14 +1,14 @@
 class ProcessUnit
   attr_accessor :init_time, :priority, :processor_time, :memory_blocks, :printer, :scanner, :modem, :num_code_disk
 
-  def initialize(params = {})
-    @init_time = params[:init_time]
-    @priority = params[:priority]
-    @processor_time = params[:processor_time]
-    @memory_blocks = params[:memory_blocks]
-    @printer = params[:printer]
-    @scanner = params[:scanner]
-    @modem = params[:modem]
-    @num_code_disk = params[:num_code_disk]
+  def initialize(init_time, priority, processor_time, memory_blocks, printer, scanner, modem, num_code_disk)
+    @init_time = init_time
+    @priority = priority
+    @processor_time = processor_time
+    @memory_blocks = memory_blocks
+    @printer = printer
+    @scanner = scanner
+    @modem = modem
+    @num_code_disk = num_code_disk
   end
 end
