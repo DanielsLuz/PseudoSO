a4df7d296aa199e8df3071fd0d8ec2de4ecb3054
DanielsLuz <danielluz1995@gmail.com>
2017-11-08 15:14:19 -0200

    Better attribute asserting.

--- spec/modules/process_unit_spec.rb
+++ spec/modules/process_unit_spec.rb
@@ -1,16 +1,19 @@
 describe ProcessUnit do
+  let(:attributes) {
+    {
+      init_time:      2,
+      priority:       0,
+      processor_time: 7,
+      memory_blocks:  64,
+      printer:        1,
+      scanner:        0,
+      modem:          0,
+      num_code_disk:  0
+    }
+  }
+
   subject { ProcessUnit.new(2, 0, 7, 64, 1, 0, 0, 0) }
 
   it { is_expected.to be_a ProcessUnit }
-
-  it "initializes values" do
-    expect(subject.init_time).to eq 2
-    expect(subject.priority).to eq 0
-    expect(subject.processor_time).to eq 7
-    expect(subject.memory_blocks).to eq 64
-    expect(subject.printer).to eq 1
-    expect(subject.scanner).to eq 0
-    expect(subject.modem).to eq 0
-    expect(subject.num_code_disk).to eq 0
-  end
+  it { is_expected.to have_attributes attributes }
 end
