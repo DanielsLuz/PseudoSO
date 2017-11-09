35e5c79584dee9bfd27225bee8c0572b6373c9a0
DanielsLuz <danielluz1995@gmail.com>
2017-11-09 15:41:42 -0200

    Added disk unit with initial operations.

---
+++ spec/modules/disk_unit_spec.rb
@@ -0,0 +1,28 @@
+describe DiskUnit do
+  let(:attributes) {
+    {
+      size: 10
+    }
+  }
+
+  subject { DiskUnit.new(10) }
+  it { is_expected.to be_a DiskUnit }
+  it { is_expected.to have_attributes attributes }
+
+  describe ".write" do
+    it "writes correctly" do
+      subject.write("X", 0, 2)
+      expect(subject[0..1]).to eq ["X", "X"]
+      expect(subject.written_blocks).to eq 2
+    end
+  end
+
+  describe ".delete" do
+    it "deletes correctly" do
+      subject.write("X", 0, 3)
+      expect(subject.written_blocks).to eq 3
+      subject.delete("X")
+      expect(subject.written_blocks).to eq 0
+    end
+  end
+end
---
+++ src/modules/disk_unit.rb
@@ -0,0 +1,34 @@
+class DiskUnit
+  attr_reader :disk
+
+  def initialize(size)
+    @disk = Concurrent::Array.new(size)
+  end
+
+  def write(data, address, size)
+    @disk[address..address + (size - 1)] = Concurrent::Array.new(size, data)
+  end
+
+  def delete(data)
+    @disk.delete(data)
+  end
+
+  def [](address)
+    @disk[address]
+  end
+
+  def size
+    @disk.size
+  end
+
+  def written_blocks
+    @disk.reject(&:nil?).count
+  end
+
+  def attributes
+    {
+      size:    size,
+      written: written
+    }
+  end
+end
