eea4452f0574c8daeee30e35616c8f0bb55edc64
DanielsLuz <danielluz1995@gmail.com>
2017-11-10 10:19:07 -0200

    Dispatches loads disk data from file.

--- spec/modules/dispatcher_spec.rb
+++ spec/modules/dispatcher_spec.rb
@@ -5,13 +5,21 @@ describe Dispatcher do
     let(:first_process) { ProcessUnit.new(2, 0, 7, 64, 1, 0, 0, 0) }
     let(:last_process) { ProcessUnit.new(3, 1, 2, 64, 0, 0, 1, 0) }
 
-    it "loads all the processes" do
+    it "loads correctly" do
       subject.load_processes(fixture_file("processes.txt"))
 
       expect(dispatcher.processes.size).to eq 3
       expect(dispatcher.processes).to all(be_a(ProcessUnit))
       expect(dispatcher.processes.first).to have_attributes first_process.attributes
       expect(dispatcher.processes.last).to have_attributes last_process.attributes
+    end
+  end
+
+  describe "#load_disk_data" do
+    it "loads correctly" do
+      subject.load_disk_data(fixture_file("files.txt"))
+      expect(subject.disk_unit.size).to eq 10
+      expect(subject.disk_unit.written_blocks).to eq 4
     end
   end
 end
--- src/modules/dispatcher.rb
+++ src/modules/dispatcher.rb
@@ -1,5 +1,5 @@
 class Dispatcher
-  attr_reader :processes
+  attr_reader :processes, :disk_unit
 
   def initialize
     @processes = Concurrent::Array.new []
@@ -8,6 +8,22 @@ class Dispatcher
   def load_processes(filename)
     File.readlines(filename).each do |line|
       @processes << ProcessUnit.new(*line.split(",").map(&:to_i))
+    end
+  end
+
+  def load_disk_data(filename)
+    lines = File.readlines(filename).map(&:strip)
+    @disk_unit = DiskUnit.new(lines.first.to_i)
+    write_operations = lines[1].to_i
+    perform_writing(lines[2, write_operations])
+  end
+
+  private
+
+  def perform_writing(operations)
+    operations.each do |operation|
+      data, address, size = operation.split(",").map(&:strip)
+      @disk_unit.write(data, address.to_i, size.to_i)
     end
   end
 end
