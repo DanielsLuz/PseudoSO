0d5ebdc800c1341e5626cefa0755df3c88a8e1a3
DanielsLuz <danielluz1995@gmail.com>
2017-11-18 23:05:06 -0200

    Spec organization.

--- spec/modules/queue_unit_spec.rb
+++ spec/modules/queue_unit_spec.rb
@@ -19,16 +19,19 @@ describe QueueUnit do
         end
 
         it "returns nil when queue is full" do
-          allow_any_instance_of(QueueUnit::PriorityQueue).to receive(:full?).and_return(true)
+          expect(queue_unit.queues[priority]).to receive(:full?).and_return(true)
           expect(queue_unit.push(process)).to eq nil
         end
       end
     end
   end
 
+  let(:process_priority_0) { ProcessUnit.new(0, 2, 0, 7, 64, 1, 0, 0, 0) }
+  let(:process_priority_1) { ProcessUnit.new(0, 2, 1, 7, 64, 1, 0, 0, 0) }
+  let(:process_priority_2) { ProcessUnit.new(0, 2, 2, 7, 64, 1, 0, 0, 0) }
+  let(:process_priority_3) { ProcessUnit.new(0, 2, 3, 7, 64, 1, 0, 0, 0) }
+
   describe "#pop" do
-    let(:process_priority_0) { ProcessUnit.new(0, 2, 0, 7, 64, 1, 0, 0, 0) }
-    let(:process_priority_1) { ProcessUnit.new(0, 2, 1, 7, 64, 1, 0, 0, 0) }
     it "returns the lowest priority process" do
       queue_unit.push(process_priority_1)
       queue_unit.push(process_priority_0)
@@ -42,9 +45,6 @@ describe QueueUnit do
   end
 
   describe "#age_queues" do
-    let(:process_priority_1) { ProcessUnit.new(0, 2, 1, 7, 64, 1, 0, 0, 0) }
-    let(:process_priority_2) { ProcessUnit.new(0, 2, 2, 7, 64, 1, 0, 0, 0) }
-    let(:process_priority_3) { ProcessUnit.new(0, 2, 3, 7, 64, 1, 0, 0, 0) }
     it "ages all queues" do
       subject.push(process_priority_1)
       subject.push(process_priority_2)
@@ -57,8 +57,6 @@ describe QueueUnit do
   end
 
   describe "#age" do
-    let(:process_priority_1) { ProcessUnit.new(0, 2, 1, 7, 64, 1, 0, 0, 0) }
-    let(:process_priority_2) { ProcessUnit.new(0, 2, 2, 7, 64, 1, 0, 0, 0) }
     it "moves process to higher priorities" do
       subject.push(process_priority_2)
       subject.age(process_priority_2)
@@ -76,13 +74,11 @@ describe QueueUnit do
   describe "can_age?" do
     it "returns true if higher priority queue has space" do
       expect(queue_unit.queues[1]).to receive(:full?).and_return(false)
-      process_priority2 = ProcessUnit.new(0, 2, 2, 7, 64, 1, 0, 0, 0)
-      expect(queue_unit.can_age?(process_priority2)).to eq true
+      expect(queue_unit.can_age?(process_priority_2)).to eq true
     end
 
     it "returns false if priority lesser than 1" do
-      process_priority1 = ProcessUnit.new(0, 2, 1, 7, 64, 1, 0, 0, 0)
-      expect(queue_unit.can_age?(process_priority1)).to eq false
+      expect(queue_unit.can_age?(process_priority_1)).to eq false
     end
   end
 
@@ -92,27 +88,25 @@ describe QueueUnit do
     it { is_expected.to have_attributes max_size: 1000, queue: an_instance_of(Concurrent::Array) }
 
     describe "#push" do
-      let(:process0) { ProcessUnit.new(0, 2, 0, 7, 64, 1, 0, 0, 0) }
       it "puts the process in the queue" do
-        subject.push(process0)
-        expect(subject.queue).to include process0
+        subject.push(process_priority_0)
+        expect(subject.queue).to include process_priority_0
       end
 
       context "when queue is full" do
         it "returns nil" do
           real_time_queue = QueueUnit::PriorityQueue.new 0, 0
-          expect(real_time_queue.push(process0)).to eq nil
-          expect(real_time_queue.queue).to_not include process0
+          expect(real_time_queue.push(process_priority_0)).to eq nil
+          expect(real_time_queue.queue).to_not include process_priority_0
         end
       end
     end
 
     describe "#pop" do
-      let(:process0) { ProcessUnit.new(0, 2, 0, 7, 64, 1, 0, 0, 0) }
       it "returns the first process" do
-        subject.push(process0)
-        expect(subject.queue.first).to eq process0
-        expect(subject.pop).to eq process0
+        subject.push(process_priority_0)
+        expect(subject.queue.first).to eq process_priority_0
+        expect(subject.pop).to eq process_priority_0
         expect(subject.queue).to be_empty
       end
     end
